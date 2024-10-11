72942f4d70401e5240c50e08f3ff0db5a3d99981
solondegwa <toppexelsam@gmail.com>
2024-10-11 22:17:16 +0300

    Add files via upload

---
+++ 1D-Array.c
@@ -0,0 +1,11 @@
+// 1D-array
+#include<stdio.h>
+int main() {
+	int i;
+    int scores [9] = {100,2,3,90,30,40,60,80,6};
+    for (i = 0;i < 8;i++) {
+    printf("scores[%d] = %d\n", i,scores [i]);
+    }
+
+   return 0;
+   }
