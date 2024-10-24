797d59a6e9dbad4f2e78b92ec5f69d16180e4585
solondegwa <toppexelsam@gmail.com>
2024-10-24 11:30:12 +0300

    Update struct.c

--- structure.c
+++ structure.c
@@ -12,29 +12,24 @@ float marks;
 
 } student1;
 int main () {
-/*
 strcpy (student1.Reg_No,"CT102/G/21749/24");
 strcpy(student1.email,"solomonndegwa800@gmail.com");
 student1.phone = 0752353064;
 student1.ID = 41128874;
 strcpy(student1.name,"solomon");
-student1.marks = 89.7;*/
+student1.marks = 89.7;
+ 
  // prompt the user to Enter
  printf("Enter the registration number:");
  scanf("%s", &student1.Reg_No);
- 
  printf("Enter the email:");
  scanf("%s", &student1.email);
- 
  printf("Enter the phone number:");
  scanf("%d", &student1.phone);
- 
  printf("Enter the ID:");
  scanf("%d", &student1.ID);
- 
  printf("Enter the the name:");
  scanf("%s", &student1.name);
- 
  printf("Enter the marks:");
  scanf("%f", &student1.marks);
  
